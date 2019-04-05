6de9d29cc088c348ffc4f39724e23fd0de0c2556
Allen, Timothy <tallen@wharton.upenn.edu>
2019-04-05 10:01:37 -0400

    More black.

--- drf_pg_builder/views.py
+++ drf_pg_builder/views.py
@@ -7,7 +7,10 @@ from pg_permissions import check_permission
 from rest_framework.permissions import BasePermission
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework_filters.backends import ComplexFilterBackend, RestFrameworkFilterBackend
+from rest_framework_filters.backends import (
+    ComplexFilterBackend,
+    RestFrameworkFilterBackend,
+)
 
 from home.pagination import estimate_count, CountEstimatePagination
 
@@ -16,7 +19,7 @@ def split_basename(basename):
     """
     Splits a base name into schema and table names.
     """
-    parts = basename.split('.')
+    parts = basename.split(".")
     schema_name = parts[0].lower()
     table_name = parts[1].lower()
 
@@ -32,11 +35,7 @@ class GenericPermission(BasePermission):
     def has_permission(self, request, view):
         schema_name, table_name = split_basename(view.basename)
 
-        return check_permission(
-            request.user.username,
-            schema_name,
-            table_name,
-        )
+        return check_permission(request.user.username, schema_name, table_name)
 
 
 class GenericViewSet(XLSXFileMixin, ReadOnlyModelViewSet):
@@ -62,21 +61,29 @@ class GenericViewSet(XLSXFileMixin, ReadOnlyModelViewSet):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         schema_name, table_name = split_basename(self.basename)
-        api_model = getattr(import_module(f'{self.app_prefix}.models.{schema_name}'), f'{table_name}_model')
-        api_serializer = getattr(import_module(f'{self.app_prefix}.serializers.{schema_name}'), f'{table_name}_serializer')
+        api_model = getattr(
+            import_module(f"{self.app_prefix}.models.{schema_name}"),
+            f"{table_name}_model",
+        )
+        api_serializer = getattr(
+            import_module(f"{self.app_prefix}.serializers.{schema_name}"),
+            f"{table_name}_serializer",
+        )
         api_permission = GenericPermission
 
         # Grab the estimated count from the query plan; if its a large table,
         # use the count estimate for Pagination instead of an exact count.
-        table_estimate_count = estimate_count(f'SELECT * FROM {schema_name}.{table_name}')
+        table_estimate_count = estimate_count(
+            f"SELECT * FROM {schema_name}.{table_name}"
+        )
         if table_estimate_count > 1000000:
             self.pagination_class = CountEstimatePagination
 
         self.queryset = api_model.objects.all()
         self.serializer_class = api_serializer
         self.permission_classes = (api_permission,)
-        self.filter_backends = (OrderingFilter, SearchFilter,)
-        self.ordering_fields = '__all__'
+        self.filter_backends = (OrderingFilter, SearchFilter)
+        self.ordering_fields = "__all__"
         self.search_fields = []
 
         # Add any columns indexed in the PostgreSQL database to be
@@ -86,7 +93,10 @@ class GenericViewSet(XLSXFileMixin, ReadOnlyModelViewSet):
         # If any columns are indexed, add the appropriate filter backends
         # and set up a dictionary of filter fields
         if len(index_columns):
-            self.filter_backends = self.filter_backends + (RestFrameworkFilterBackend, ComplexFilterBackend,)
+            self.filter_backends = self.filter_backends + (
+                RestFrameworkFilterBackend,
+                ComplexFilterBackend,
+            )
             self.filter_fields = {}
 
         # Loop through all of the fields. If the field is indexed, add it
@@ -95,25 +105,29 @@ class GenericViewSet(XLSXFileMixin, ReadOnlyModelViewSet):
         for field in api_model._meta.get_fields():
             if field.name in index_columns:
                 field_type = field.get_internal_type()
-                if field_type in ('CharField', 'TextField'):
+                if field_type in ("CharField", "TextField"):
                     # Add column to searchable fields, with 'starts with' search ('^')
                     # See: http://www.django-rest-framework.org/api-guide/filtering/#searchfilter
-                    self.search_fields.append(f'^{field.name}')
+                    self.search_fields.append(f"^{field.name}")
 
                     # Add column to filterable fields with all search options
                     self.filter_fields[field.name] = [
-                        'exact', 'contains', 'startswith', 'endswith',
+                        "exact",
+                        "contains",
+                        "startswith",
+                        "endswith",
                     ]
-                elif field_type in ('IntegerField', 'BigIntegerField', 'DecimalField', 'FloatField'):
+                elif field_type in (
+                    "IntegerField",
+                    "BigIntegerField",
+                    "DecimalField",
+                    "FloatField",
+                ):
                     # Add column to filterable fields with all search options
-                    self.filter_fields[field.name] = [
-                        'exact', 'lt', 'lte', 'gt', 'gte',
-                    ]
-                elif field_type in ('DateField', 'DateTimeField', 'TimeField'):
+                    self.filter_fields[field.name] = ["exact", "lt", "lte", "gt", "gte"]
+                elif field_type in ("DateField", "DateTimeField", "TimeField"):
                     # Add column to filterable fields with all search options
-                    self.filter_fields[field.name] = [
-                        'exact', 'lt', 'lte', 'gt', 'gte',
-                    ]
+                    self.filter_fields[field.name] = ["exact", "lt", "lte", "gt", "gte"]
 
         self.search_fields = tuple(self.search_fields)
 
@@ -123,13 +137,10 @@ class GenericViewSet(XLSXFileMixin, ReadOnlyModelViewSet):
         by providing schema name and table name.
         """
 
-        cursor = connections['pgdata'].cursor()
+        cursor = connections["pgdata"].cursor()
 
         cursor.execute(
-            self.index_sql, {
-                "table_schema": schema_name,
-                "table_name": table_name,
-            }
+            self.index_sql, {"table_schema": schema_name, "table_name": table_name}
         )
 
         rows = cursor.fetchall()
